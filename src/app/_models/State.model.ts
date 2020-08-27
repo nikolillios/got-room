ff2fa86cfb04dd9b75eed6cb80fed4e83bff85e9
ncl44 <ncl44@cornell.edu>
2020-08-27 18:32:07 -0400

    state model change

--- src/app/_models/State.model.ts
+++ src/app/_models/State.model.ts
@@ -5,13 +5,11 @@ export class State {
   public tiles: Array<Array<Tile>>;
   public players: Array<Player>;
   public gameStarted: boolean;
-  public score: Array<number>;
   public turn: number;
   constructor() {
     this.tiles = [];
     this.players = [];
     this.gameStarted = false;
-    this.score = [];
     this.turn = 0;
   }
 }
